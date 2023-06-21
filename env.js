e75b5b537d256e7f5beac2b5f9252c0f5e288491
方铭飞 <70510121@qq.com>
2023-06-21 17:23:54 +0800

    ok

--- env.js
+++ env.js
@@ -1,2 +1,5 @@
 // export const API_URL = 'https://yaji.shuchuang.net/api/hey/';
-export const API_URL = 'https://yaji.ixiaojin.cn/api/hey/';
+
+
+// export const API_URL = 'https://yaji.ixiaojin.cn/api/hey/'; //测试
+export const API_URL = 'https://yaji.suoeryoude.cn/api/hey/'; //正式
