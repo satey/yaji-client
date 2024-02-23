5f581f7bdf4e246e3ba9fd36333ace567a4324c6
方铭飞 <70510121@qq.com>
2024-02-23 20:40:47 +0800

    2.0.5

--- components/fei-renew/fei-renew.vue
+++ components/fei-renew/fei-renew.vue
@@ -1,17 +1,37 @@
 <template>
 	<view>
-		<u-modal :show="isRenew" :showConfirmButton="true" :background="'#fff'"
-			:showCancelButton="enforce==1?false:true" confirmColor="#FFA000" @confirm="download" @cancel="renewCancel">
-			<view>
-				<view class="renewTitle">更新内容</view>
-				<scroll-view scroll-y="true" class="myScroll" style="margin-top: 30rpx;" v-if="progress">
-					<view v-html="renewContent"></view>
-				</scroll-view>
-				<view v-if="!progress" style="margin-top: 30rpx;">
-					<progress :percent="progressNum" show-info style="width: 500rpx;" />
+		<block v-if="platform == 'android'">
+			<u-modal :show="isRenew" :showConfirmButton="true" :background="'#fff'"
+				:showCancelButton="enforce==1?false:true" confirmColor="#FFA000" @confirm="download"
+				@cancel="isRenew = false">
+				<view>
+					<view class="renewTitle">更新内容</view>
+					<scroll-view scroll-y="true" class="myScroll" style="margin-top: 30rpx;" v-if="progress">
+						<view v-html="renewContent"></view>
+					</scroll-view>
+					<view v-if="!progress" style="margin-top: 30rpx;">
+						<progress :percent="progressNum" show-info style="width: 500rpx;" />
+					</view>
 				</view>
-			</view>
-		</u-modal>
+			</u-modal>
+		</block>
+
+		<block v-if="platform== 'ios'">
+			<u-modal :show="isRenew" :showConfirmButton="true" :confirmText="'前往更新'" :background="'#fff'"
+				:showCancelButton="enforce==1?false:true" confirmColor="#FFA000" @confirm="openAppStore"
+				@cancel="isRenew = false">
+				<view>
+					<view class="renewTitle">更新内容</view>
+					<scroll-view scroll-y="true" class="myScroll" style="margin-top: 30rpx;" v-if="progress">
+						<view v-html="renewContent"></view>
+					</scroll-view>
+					<view v-if="!progress" style="margin-top: 30rpx;">
+						<progress :percent="progressNum" show-info style="width: 500rpx;" />
+					</view>
+				</view>
+			</u-modal>
+		</block>
+
 	</view>
 </template>
 
@@ -27,61 +47,61 @@
 				progress: true,
 				progressNum: 0,
 				downloadFlag: true,
-				phoneMode: uni.getSystemInfoSync().platform,
+				iosAppStoraId: '',
+				platform: uni.getSystemInfoSync().platform
 			}
 		},
 		created() {
 			var that = this;
-			that.$api("versions.index").then(res => {
-				if (res.data == null) {
-					return;
-				}
-				// #ifdef APP-PLUS
-				if (this.phoneMode == "android") {
-					plus.runtime.getProperty(plus.runtime.appid, (info) => {
-						if (info.version != res.data.newversion) {
-							that.isRenew = true;
-							that.enforce = res.data.enforce;
-							that.downloadUrl = res.data.downloadurl;
-							that.renewContent = res.data.content
-						}
-					})
-				}
-				// #endif
+			// #ifdef APP-PLUS
+			plus.runtime.getProperty(plus.runtime.appid, (info) => {
+				console.log(info.version)
+				that.$api("versions.index", {
+					version: info.version
+				}).then(res => {
+					console.log(res)
+					if (res.data == null) {
+						return;
+					}
+					that.isRenew = true;
+					that.enforce = res.data.enforce;
+					that.downloadUrl = res.data.downloadurl;
+					that.renewContent = res.data.content
+					that.iosAppStoraId = Number(res.data.ios_appstore_id)
+				})
 			})
+			// #endif
 		},
 		methods: {
-			//关闭更新弹窗
-			renewCancel() {
-				this.isRenew = false
+			//跳转appStora
+			openAppStore() {
+				var that = this;
+				// #ifdef APP
+				plus.runtime.launchApplication({
+					action: `itms-apps://itunes.apple.com/cn/app/id${ that.iosAppStoraId}?mt=8`
+				}, function(e) {
+					console.log('Open system default browser failed: ' + e.message);
+				});
+				// #endif
 			},
 			//下载新版本
 			download() {
 				var that = this;
-				if (uni.getSystemInfoSync().platform == "ios") {
-					// plus.runtime.launchApplication({
-					// 	action: `itms-apps://itunes.apple.com/cn/app/hello-uni-app/id1417078253?mt=8`
-					// }, function(e) {
-					// 	console.log('Open system default browser failed: ' + e.message);
-					// });
-				} else {
+				if (uni.getSystemInfoSync().platform == "android") {
 					if (that.downloadFlag) {
-						this.progress = false;
+						that.progress = false;
 						that.downloadFlag = false;
 						var downloadTask = uni.downloadFile({
-							url: that.$store.state.renew.downloadUrl,
+							url: that.downloadUrl,
 							success: function(res) {
-								uni.hideLoading()
 								// 安装新版本
 								uni.showModal({
 									title: '安装新版本',
 									content: '新版本已下载完成，是否安装？',
 									success: function(msg) {
 										if (msg.confirm) {
-											console.log(that.downloadFlag)
 											if (that.downloadFlag) {
 												// #ifdef APP-PLUS
-
 												plus.runtime.install(res.tempFilePath);
 												// #endif
 											}
